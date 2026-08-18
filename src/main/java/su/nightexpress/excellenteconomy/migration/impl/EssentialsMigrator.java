dd0c8ba41d1cd49620fd2f17287f31f3a14008ab
Chris <90093334+Chwitst187@users.noreply.github.com>
2026-08-18 15:50:04 +0200

    feat: read Essentials balances locally from userdata ymls

--- src/main/java/su/nightexpress/excellenteconomy/migration/impl/EssentialsMigrator.java
+++ src/main/java/su/nightexpress/excellenteconomy/migration/impl/EssentialsMigrator.java
@@ -28,6 +28,37 @@ public class EssentialsMigrator extends Migrator {
     @NotNull
     public Map<OfflinePlayer, Double> getBalances(@NotNull ExcellentCurrency currency) {
         Map<OfflinePlayer, Double> balances = new HashMap<>();
+
+        java.io.File essentialsFolder = new java.io.File(this.plugin.getDataFolder().getParentFile(), "Essentials");
+        java.io.File userdataFolder = new java.io.File(essentialsFolder, "userdata");
+        if (userdataFolder.exists() && userdataFolder.isDirectory()) {
+            java.io.File[] files = userdataFolder.listFiles((dir, name) -> name.endsWith(".yml"));
+            if (files != null) {
+                for (java.io.File file : files) {
+                    try {
+                        String nameWithoutExt = file.getName().substring(0, file.getName().length() - 4);
+                        UUID uuid = UUID.fromString(nameWithoutExt);
+                        org.bukkit.configuration.file.YamlConfiguration config = org.bukkit.configuration.file.YamlConfiguration.loadConfiguration(file);
+                        Object moneyObj = config.get("money");
+                        if (moneyObj != null) {
+                            double money;
+                            if (moneyObj instanceof Number) {
+                                money = ((Number) moneyObj).doubleValue();
+                            } else {
+                                money = Double.parseDouble(moneyObj.toString().replace(",", ""));
+                            }
+                            OfflinePlayer offlinePlayer = this.plugin.getServer().getOfflinePlayer(uuid);
+                            balances.put(offlinePlayer, money);
+                        }
+                    } catch (IllegalArgumentException ignored) {
+                    } catch (Exception exception) {
+                        this.plugin.error("Could not convert Essentials balance from file '" + file.getName() + "'!");
+                        exception.printStackTrace();
+                    }
+                }
+            }
+        }
+
         Essentials essentials = (Essentials) this.getBackend();
         if (essentials == null) return balances;
 
